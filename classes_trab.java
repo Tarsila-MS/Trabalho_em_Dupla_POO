c44211c2d98827e761f87d4daa3dd8f74227cda5
Tarsila-MS <tarsilamsouza@gmail.com>
2021-03-21 12:21:47 -0300

    Create classes_trab.java

---
+++ classes_trab.java
@@ -0,0 +1,70 @@
+class Cliente{
+
+private double valorDaDivida;
+
+public void setCliente (double valorDaDivida){
+	   this.valorDaDivida = valorDaDivida 	
+	}
+}
+
+public class Pessoa{
+
+	protect String nome, cpf;
+
+	public Pessoa(String nome, String cpf){
+		
+	}
+}
+
+public class Empregado{
+
+	protect String matricula;
+	
+	public void setEmpregado (String matricula){
+	   this.matricula = matricula	
+	}
+	
+}
+
+public class Gerente(double salario, double bonus){
+
+                private double salario;
+                
+                private double bonus{
+                        
+	}
+	
+}
+public interface Bonus{
+
+        public CalcularBonus(){
+        
+                return salario * bonus;
+        }
+                
+}
+
+public class Vendedor(double totalDasVendas, double comissão){
+
+                private double totalDasVendas;
+                
+                private double comissão{
+                        
+	}
+	
+}
+
+public interface Salario{
+        
+        public calcularSalario(){
+        
+                return totalDeVendas * comissao;
+        }
+        
+
+              
+}
+
+}
+
+
