9ed704c806e43ef0041c2a37d11dcf38a6caab88
Tarsila-MS <tarsilamsouza@gmail.com>
2021-03-26 15:58:33 -0300

    Create Gerente.java

---
+++ Gerente.java
@@ -0,0 +1,15 @@
+public class Gerente extends Empregado {
+
+			public Gerente(String nome, String cpf) {
+				super(nome, cpf);
+
+			}
+
+			private double salario;
+
+			private double bonus;
+			{
+
+			}
+
+		}
